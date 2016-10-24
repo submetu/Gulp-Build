6ffc6c4bbfd4883854b3ddf44968a77eb4971c58
M Subhan Naeem <sub.metu@gmail.com>
2016-10-24 20:56:35 +0300

    Added javascript concat and minification

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,16 @@ var gulp = require('gulp'),
 	rename = require('gulp-rename');
 
 gulp.task('concatScripts',function(){
+	return gulp.src(['js/circle/autogrow.js',
+			  'js/circle/circle.js'])
+	.pipe(concat('global.js'))
+	.pipe(gulp.dest('js'));
+});
+gulp.task('minifyScripts',['concatScripts'],function(){
+	return gulp.src('js/global.js')
+	.pipe(uglify())
+	.pipe(rename('all.min.js'))
+	.pipe(gulp.dest('dist/scripts'));
 });
 
 
