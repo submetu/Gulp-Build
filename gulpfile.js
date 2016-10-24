0faade86598a81635e425ab386424db5ea8da9fa
M Subhan Naeem <sub.metu@gmail.com>
2016-10-24 19:54:04 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename');
+
+gulp.task('concatScripts',function(){
+});
+
+
+
+
+
+gulp.task('default',function(){
+
+});
